e4efb6dd1a92831a209fe89061605c789cfb242d
erik-epicodus <67275977+erik-epicodus-help@users.noreply.github.com>
2020-06-23 09:51:08 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// import { pingPong } from './ping-pong';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function () {
+    // $('#submit-button').submit(function (event) {
+    //     event.preventDefault();
+
+    // });
+});
