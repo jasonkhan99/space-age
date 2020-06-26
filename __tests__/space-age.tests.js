ec1c65d59b1c4ddad17e110a5eedc5b7240f9544
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-26 09:49:17 -0700

    run test on user object to reach expectation and pass

--- __tests__/space-age.tests.js
+++ __tests__/space-age.tests.js
@@ -4,7 +4,7 @@ describe('User and all its properties', () => {
 
   test('should correctly create a User object with three properties: age, country, sex', () => {
     const user = new User(25, "Mexico", "Female");
-    expect(user.age).toEqual(50);
+    expect(user.age).toEqual(25);
     expect(user.country).toEqual("Mexico");
     expect(user.sex).toEqual("Female");
   });
