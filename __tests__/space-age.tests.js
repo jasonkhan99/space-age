6bf2e8fbe3762063e73ab010af7ecfaa142ea85c
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-26 10:01:35 -0700

    use a before each to DRY up tests

--- __tests__/space-age.tests.js
+++ __tests__/space-age.tests.js
@@ -1,9 +1,13 @@
 import { User } from '../src/space-age.js';
 
 describe('User and all its properties', () => {
+  let user;
+
+  beforeEach(() => {
+    user = new User(25, "Mexico", "Female");
+  });
 
   test('should correctly create a User object with three properties: age, country, sex', () => {
-    const user = new User(25, "Mexico", "Female");
     expect(user.age).toEqual(25);
     expect(user.country).toEqual("Mexico");
     expect(user.sex).toEqual("Female");
